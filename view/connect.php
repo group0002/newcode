36e057ef0c82b0878a27fee614f4cced371b45a7
nguyendo <thanhdo0987698050@gmail.com>
2019-07-20 10:49:42 -0400

    new

---
+++ view/connect.php
@@ -0,0 +1,14 @@
+<?php
+$server_username = "nguyendo";
+$server_password = "27081999";
+$server_host = "localhost";
+$database = 'project_group2';
+
+$conn = mysqli_connect($server_host,$server_username,$server_password,$database);
+mysqli_select_db($conn,'project_group2');
+
+
+
+
+
+
